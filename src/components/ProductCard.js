deae3fc882091b0f673332c520c455b8350e08e1
Joao Pouey <jcpouey@gmail.com>
2024-12-27 11:54:22 -0300

    correção da responsividade em telas xl e mobile

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -9,31 +9,31 @@ const ProductCard = ({
   amazonLink,
 }) => {
   return (
-    <div className="grid sm:flex sm:flex-cols-2 items-start text-left mb-4">
-      <div className="relative grid grid-cols-2 bg-[#002642] w-[350px] h-40 sm:w-[692px] sm:h-[285px] rounded-lg mt-4">
-        <div className="flex flex-col justify-center sm:justify-start sm:mt-14 pl-7 text-center">
+    <div className="grid xl:flex xl:flex-cols-2 items-start text-left mb-4">
+      <div className="relative grid grid-cols-2 bg-[#002642] w-[350px] h-40 xl:w-[692px] xl:h-[285px] rounded-lg mt-4">
+        <div className="flex flex-col justify-center xl:justify-start xl:mt-14 pl-7 text-center">
           <p className="text-[#CCFF00] text-2xl font-['Montserrat'] font-bold">
             {rank}
           </p>
-          <p className="text-white text-md font-['Montserrat'] font-medium sm:pt-1">
+          <p className="text-white text-md font-['Montserrat'] font-medium xl:pt-1">
             Avaliação. {rating}
           </p>
-          <p className="text-white text-sm sm:text-lg sm:pt-4 font-['Montserrat'] font-normal mt-8">
+          <p className="text-white xl:text-lg xl:pt-4 font-['Montserrat'] font-normal mt-8">
             {productName}
           </p>
         </div>
-        <div className="absolute h-40 sm:h-[285px] w-40 sm:w-[300px] right-0 rounded-r-lg bg-white flex justify-center items-center">
-          <img src={imageSrc} alt={productName} className="h-32 sm:h-[230px]" />
+        <div className="absolute h-40 xl:h-[285px] w-40 xl:w-[300px] right-0 rounded-r-lg bg-white flex justify-center items-center">
+          <img src={imageSrc} alt={productName} className="h-32 xl:h-[230px]" />
         </div>
       </div>
-      <div className="w-[350px] font-[Montserrat] grid pt-2 sm:pt-0 grid-cols-2 sm:grid-cols-1 sm:ml-4 sm:text-sm sm:mt-[16px]">
+      <div className="w-[350px] font-[Montserrat] grid pt-2 xl:pt-0 grid-cols-2 xl:grid-cols-1 xl:ml-4 xl:text-xl xl:mt-[16px]">
         <div>
-          <p className="font-medium text-md sm:mb-2 sm:mt-6">
+          <p className="font-medium text-sm xl:mb-2 xl:mt-6">
             Características especiais:
           </p>
-          <p className="text-sm sm:text-xs">{features}</p>
+          <p className="text-xs">{features}</p>
         </div>
-        <div className="flex flex-col items-center my-auto sm:mt-16">
+        <div className="flex flex-col items-center my-auto xl:mt-16">
           <a href={amazonLink} target="_blank" rel="noopener noreferrer">
             <button className="w-[148px] h-10 bg-[#002642] rounded-[20px] px-[23px] py-2.5 mt-4 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] items-center inline-flex">
               <div className="text-[#ccff00] text-base font-medium font-['Roboto'] underline">
