01b66c9bbb3ff9239a70d2f9c9e97c6ad859106c
Joao Pouey <jcpouey@gmail.com>
2024-12-29 14:16:27 -0300

    ultimos ajustes feitos nos produtos

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -9,26 +9,30 @@ const ProductCard = ({
   amazonLink,
 }) => {
   return (
-    <div className="grid md:flex md:flex-cols-2 items-start text-left mb-4">
-      <div className="relative grid grid-cols-2 bg-[#002642] w-[350px] h-40 md:w-[406px] md:h-[169px] xl:w-[692px] xl:h-[285px] rounded-lg mt-4">
-        <div className="flex flex-col justify-center xl:justify-start xl:mt-14 pl-7 text-center">
-          <p className="text-[#CCFF00] text-2xl font-['Montserrat'] font-bold">
+    <div className="md:flex md:flex-cols-2 items-start text-left mb-4">
+      <div className="relative grid grid-cols-1 sm:grid-cols-2 bg-[#002642] w-[355px] sm:w-[350px] sm:h-40 md:w-[406px] h-[548px] md:h-[169px] xl:w-[692px] xl:h-[285px] rounded-2xl mt-4">
+        <div className="flex flex-col justify-start sm:justify-center xl:justify-start xl:mt-14 pl-0 sm:pl-7 pt-8 sm:pt-0 text-center">
+          <p className="text-[#CCFF00] text-4xl sm:text-2xl font-['Montserrat'] font-bold">
             {rank}
           </p>
           <p className="text-white text-md font-['Montserrat'] font-medium xl:pt-1">
             Avaliação. {rating}
           </p>
-          <p className="text-white xl:text-lg xl:pt-4 font-['Montserrat'] font-normal mt-8">
+          <p className="text-white text-xl xl:text-lg xl:pt-4 font-['Montserrat'] font-normal pt-14 sm:pt-8">
             {productName}
           </p>
         </div>
-        <div className="absolute h-40 xl:h-[285px] md:h-[169px] w-40 sm:w-[162px] xl:w-[300px] right-0 rounded-r-lg bg-white flex justify-center items-center">
-          <img src={imageSrc} alt={productName} className="h-32 xl:h-[230px]" />
+        <div className="absolute h-[281px] sm:h-40 md:h-[169px] xl:h-[285px] w-[355px] sm:w-40 md:w-[162px] xl:w-[300px] bottom-0 sm:right-0 rounded-b-2xl sm:rounded-r-2xl bg-white flex justify-center items-center">
+          <img
+            src={imageSrc}
+            alt={productName}
+            className="h-60 sm:h-32 xl:h-56"
+          />
         </div>
       </div>
-      <div className="w-[350px] font-[Montserrat] grid pt-2 xl:pt-0 grid-cols-2 md:grid-cols-1 md: ml-2 xl:ml-4 xl:text-xl xl:mt-[16px]">
+      <div className="w-[350px] font-[Montserrat] grid grid-cols-1 sm:grid-cols-2 pt-2 md:grid-cols-1 xl:pt-0 ml-4 md:ml-2 xl:ml-4 xl:text-xl xl:mt-[16px]">
         <div>
-          <p className="font-medium text-sm md:mb-2 md:mt-4 xl:mt-6">
+          <p className="font-medium text-md md:mb-2 md:mt-4 xl:mt-6">
             Características especiais:
           </p>
           <p className="text-xs">{features}</p>
