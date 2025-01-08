72efd87214309be76180811d71b821d98fdfba8a
joao <jcpouey@gmail.com>
2025-01-08 17:42:31 -0300

    ajuste na responsividade da newsletter em telas grandes

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Newsletter = () => {
   return (
-    <section className="flex bg-[#002642] text-white py-8 md:py-0 px-4 m-4 mt-8 rounded-[10px] flex-col md:flex-row md:h-28 md:justify-center">
+    <section className="flex bg-[#002642] text-white py-8 md:py-0 px-4 m-4 mt-8 rounded-[10px] flex-col md:flex-row md:h-28 md:justify-center lg:justify-around">
       {/* grid 1 - Input e botão */}
       <div className="flex w-[100%] md:w-96 h-14 bg-gray-200 border-2 border-[#ccff00] rounded-lg px-4 py-2 md:mt-6 lg:mx-[7%]">
         <input
@@ -16,7 +16,7 @@ const Newsletter = () => {
       </div>
 
       {/* grid 2 - Texto da newsletter */}
-      <div className="flex flex-col mt-4 w-[100%] md:w-96 lg:w-[505px] sm:mt-4 sm:ml-2 md:mt-2 md:ml-6 lg:mt-3">
+      <div className="flex flex-col mt-4 w-[100%] md:w-96 lg:w-[505px] sm:mt-4 sm:ml-2 md:mt-2 md:ml-6 lg:mt-3 xl:w-[600px] xl:pt-4">
         <span className="text-[#f2f2f2] text-xl font-light font-['Montserrat'] tracking-tight">
           Assine nossa newsletter e faça a escolha certa sempre.
         </span>{' '}
--- src/data/product_data.js
+++ src/data/product_data.js
@@ -68,45 +68,6 @@ const productData = [
       },
     ],
   },
-
-  // Fone Bluetooth
-  {
-    id: 2,
-    category: 'fone_bluetooth',
-    name: 'FONES BLUETOOTH',
-    image: '/categorias/fone-bluetooth.png',
-    subtext:
-      'Confira as dicas para adquirir o melhor fone bluetooth e ainda uma lista com os produtos mais bem avaliados da categoria no site Amazon.com',
-    products: [
-      {
-        rank: 'TOP 1',
-        imageSrc: '/foto-produtos/robo-1-xiaomi-mop2.jpg',
-        productName: 'Fone Bluetooth Modelo 1',
-        rating: '9,5',
-        features:
-          'Som de alta qualidade, design confortável, bateria de longa duração.',
-        amazonLink: 'https://www.amazon.com',
-      },
-      {
-        rank: 'TOP 2',
-        imageSrc: '/foto-produtos/robo-1-xiaomi-mop2.jpg',
-        productName: 'Fone Bluetooth Modelo 2',
-        rating: '9,3',
-        features:
-          'Cancelamento de ruído, microfone integrado, resistente à água.',
-        amazonLink: 'https://www.amazon.com',
-      },
-      {
-        rank: 'TOP 3',
-        imageSrc: '/foto-produtos/robo-1-xiaomi-mop2.jpg',
-        productName: 'Fone Bluetooth Modelo 2',
-        rating: '9,3',
-        features:
-          'Cancelamento de ruído, microfone integrado, resistente à água.',
-        amazonLink: 'https://www.amazon.com',
-      },
-    ],
-  },
 ];
 
 export default productData;
